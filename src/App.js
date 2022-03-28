c813e7daa7589e575f84420a122381a8bb0edc96
mathias labora martirena <mathiaslabora@gmail.com>
2022-03-28 00:34:16 -0300

    u

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import Home from "./container/Home";
-import {Routes, Route} from "react-router-dom";
+import {Router, Route} from "react-router-dom";
 import Capitles from './container/Capitles';
 
 function App() {
   return(
 
-    <Routes basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
     <Route path="/" elemeprocessnt={<Home />} />
     <Route path="/capitles" element={<Capitles />} />
-  </Routes>
+  </Router>
 
   )
 }
